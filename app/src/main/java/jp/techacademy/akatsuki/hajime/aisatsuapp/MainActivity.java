4ffc71e1098fa4247f95706dc9c97ccebb9fdafb
komainu-22 <hajime.akatsuki@prdm.biz>
2018-08-09 03:55:53 +0900

    画面内にButtonとTextViewを配置してください。 ButtonをタッチするとTimePickerDialogを表示して，設定した時刻に応じてTextViewに表示されるあいさつを変化させてください。 2:00 ~ 9:59 では「おはよう」 10:00 ~ 17:59 では「こんにちは」 18:00 ~ 1:59 では「こんばんは」

--- app/src/main/java/jp/techacademy/akatsuki/hajime/aisatsuapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/akatsuki/hajime/aisatsuapp/MainActivity.java
@@ -1,13 +1,63 @@
 package jp.techacademy.akatsuki.hajime.aisatsuapp;
 
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v){
+        if (v.getId() == R.id.button1) {
+            showTimePickerDialog();
+        }
+        mTextView = (TextView)findViewById(R.id.textView);
+    }
+
+
+    private void showTimePickerDialog(){
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Log.d("UI-PARTS",String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
+                        if (hourOfDay < 10 & hourOfDay >= 2 ){
+                            mTextView.setText("おはよう");
+                        }else if (hourOfDay < 18 & hourOfDay >=10){
+                            mTextView.setText("こんにちは");
+                        }else {
+                            mTextView.setText("こんばんは");
+                        }
+                    }
+                },
+                0,
+                0,
+                true
+        );
+
+
+        timePickerDialog.show();
+
+    }
+
+
+
 }
